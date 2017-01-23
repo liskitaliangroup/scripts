eb156b524efb78b3889a7a7cd6bd57a90340ba97
Davide Gessa <gessadavide@gmail.com>
2017-01-23 10:33:25 +0100

    Update scripts

---
+++ init.sh
@@ -0,0 +1,63 @@
+# dakk
+git clone git@github.com:liskitaliangroup/liskapi.git ../liskapi
+cd ../liskapi
+git remote add upstream git@github.com:dakk/liskapi.git
+cd ../scripts
+
+git clone git@github.com:liskitaliangroup/lisk-dump-detector.git ../lisk-dump-detector
+cd ../lisk-dump-detector
+git remote add upstream git@github.com:dakk/lisk-dump-detector.git
+cd ../scripts
+
+git clone git@github.com:liskitaliangroup/lisk-dice.git ../lisk-dice
+cd ../lisk-dice
+git remote add upstream git@github.com:dakk/lisk-dice.git
+cd ../scripts
+
+git clone git@github.com:liskitaliangroup/lisk-delegate-monitor.git ../lisk-delegate-monitor
+cd ../lisk-delegate-monitor
+git remote add upstream git@github.com:dakk/lisk-delegate-monitor.git
+cd ../scripts
+
+git clone git@github.com:liskitaliangroup/termux-liskmonitor.git ../termux-liskmonitor
+cd ../termux-liskmonitor
+git remote add upstream git@github.com:dakk/termux-liskmonitor.git
+cd ../scripts
+
+git clone git@github.com:liskitaliangroup/lisk-italian-group.git ../lisk-italian-group
+cd ../lisk-italian-group
+git remote add upstream git@github.com:dakk/lisk-italian-group.git
+cd ../scripts
+
+# andreafspeziale
+git clone git@github.com:liskitaliangroup/liskitbot.git ../liskitbot
+cd ../liskitbot
+git remote add upstream git@github.com:andreafspeziale/liskitbot.git
+cd ../scripts
+
+git clone git@github.com:liskitaliangroup/lisk-explorer.git ../lisk-explorer
+cd ../lisk-explorer
+git remote add upstream git@github.com:andreafspeziale/lisk-explorer.git
+cd ../scripts
+
+git clone git@github.com:liskitaliangroup/lisk-ansible.git ../lisk-ansible
+cd ../lisk-ansible
+git remote add upstream git@github.com:andreafspeziale/lisk-ansible.git
+cd ../scripts
+
+git clone git@github.com:liskitaliangroup/liskitbash.git ../liskitbash
+cd ../liskitbash
+git remote add upstream git@github.com:andreafspeziale/liskitbash.git
+cd ../scripts
+
+git clone git@github.com:liskitaliangroup/lisk-js-autorebuilder.git ../lisk-js-autorebuilder
+cd ../lisk-js-autorebuilder
+git remote add upstream git@github.com:andreafspeziale/lisk-js-autorebuilder.git
+cd ../scripts
+
+
+# corsaro1
+git clone git@github.com:liskitaliangroup/wallisk.git ../wallisk
+cd ../wallisk
+git remote add upstream git@github.com:corsaro1/wallisk.git
+cd ../scripts
---
+++ update.sh
@@ -0,0 +1,80 @@
+cd ../liskapi 
+git fetch upstream 
+git rebase upstream/master 
+git push -u origin master
+cd ../scripts
+
+cd ../lisk-dump-detector 
+git fetch upstream 
+git rebase upstream/master 
+git push -u origin master
+cd ../scripts
+
+cd ../lisk-dice 
+git fetch upstream 
+git rebase upstream/master 
+git push -u origin master
+cd ../scripts
+
+cd ../lisk-delegate-monitor 
+git fetch upstream 
+git rebase upstream/master 
+git push -u origin master
+cd ../scripts
+
+cd ../termux-liskmonitor 
+git fetch upstream 
+git rebase upstream/master 
+git push -u origin master
+cd ../scripts
+
+cd ../lisk-italian-group 
+git fetch upstream 
+git rebase upstream/master 
+git push -u origin master
+cd ../scripts
+
+cd ../liskitaliangroup.github.io
+cp -rf ../lisk-italian-group/docs/* ./
+git add *
+git commit -m "site bump"
+git push -u origin master
+cd ../scripts
+
+cd ../liskitbot 
+git fetch upstream 
+git rebase upstream/master 
+git push -u origin master
+cd ../scripts
+
+cd ../lisk-explorer 
+git fetch upstream 
+git rebase upstream/master 
+git push -u origin master
+cd ../scripts
+
+cd ../lisk-ansible 
+git fetch upstream 
+git rebase upstream/master 
+git push -u origin master
+cd ../scripts
+
+cd ../liskitbash 
+git fetch upstream 
+git rebase upstream/master 
+git push -u origin master
+cd ../scripts
+
+cd ../lisk-js-autorebuilder 
+git fetch upstream 
+git rebase upstream/master 
+git push -u origin master
+cd ../scripts
+
+cd ../wallisk 
+git fetch upstream 
+git rebase upstream/master 
+git push -u origin master
+cd ../scripts
+
+
